f03ffcfa28865b0c1242a30adc130cf7867152a5
cynthiakar <duckie888@gmail.com>
2019-04-22 20:52:31 -0400

    updated spacing

--- dgemm.c
+++ dgemm.c
@@ -55,7 +55,8 @@ void dgemm_unrolling(int n)
 void do_block(int n, int si, int sj, int sk, double *a, double *b, double *c)
 {
     int i, j, k;
-    for (i = si; i < si + BLOCK_SIZE; i++)     for (j = sj; j < sj + BLOCK_SIZE; j++)
+    for (i = si; i < si + BLOCK_SIZE; i++)     
+        for (j = sj; j < sj + BLOCK_SIZE; j++)
         {
             double cij = c[i * n + j];
             for (k = sk; k < sk + BLOCK_SIZE; k++) cij += a[i * n + k] * b[k * n + j];
@@ -65,10 +66,12 @@ void do_block(int n, int si, int sj, int sk, double *a, double *b, double *c)
 void dgemm_blocking(int n)
 {
     int i, j, k;
-    for(i = 0; i < n; i += BLOCK_SIZE)     for(j = 0; j < n; j += BLOCK_SIZE)
+    for(i = 0; i < n; i += BLOCK_SIZE)     
+        for(j = 0; j < n; j += BLOCK_SIZE)
         {
             c[i * n + j] = 0;
-            for(k = 0; k < n; k += BLOCK_SIZE)           do_block(n, i, j, k, a, b, c);
+            for(k = 0; k < n; k += BLOCK_SIZE)           
+                do_block(n, i, j, k, a, b, c);
         }
 }
 
