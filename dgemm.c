f3dfd31eb6d47b5217bb35899ab832af81ec5799
Andrew <andrewhamann1999@gmail.com>
2019-04-23 15:42:47 -0400

    added beginning to loop unrolling + SIMD mechanism

--- dgemm.c
+++ dgemm.c
@@ -57,11 +57,11 @@ void dgemm_unrolling(int n)
 void do_block(int n, int si, int sj, int sk, double *a, double *b, double *c)
 {
     int i, j, k;
-    for (i = si; i < si + BLOCK_SIZE; i++)     
+    for (i = si; i < si + BLOCK_SIZE; i++)
         for (j = sj; j < sj + BLOCK_SIZE; j++)
         {
             double cij = c[i * n + j];
-            for (k = sk; k < sk + BLOCK_SIZE; k++) 
+            for (k = sk; k < sk + BLOCK_SIZE; k++)
                 cij += a[i * n + k] * b[k * n + j];
             c[i * n + j] = cij;
         }
@@ -69,11 +69,11 @@ void do_block(int n, int si, int sj, int sk, double *a, double *b, double *c)
 void dgemm_blocking(int n)
 {
     int i, j, k;
-    for(i = 0; i < n; i += BLOCK_SIZE)     
+    for(i = 0; i < n; i += BLOCK_SIZE)
         for(j = 0; j < n; j += BLOCK_SIZE)
         {
             c[i * n + j] = 0;
-            for(k = 0; k < n; k += BLOCK_SIZE)           
+            for(k = 0; k < n; k += BLOCK_SIZE)
                 do_block(n, i, j, k, a, b, c);
         }
 }
@@ -103,7 +103,7 @@ void dgemm_intrin(int n)
 void do_block_SIMD(int n, int si, int sj, int sk, double *a, double *b, double *c)
 {
     int i, j, k;
-    for (i = si; i < si + BLOCK_SIZE; i++)     
+    for (i = si; i < si + BLOCK_SIZE; i++)
         for (j = sj; j < sj + BLOCK_SIZE; j+=4) {
             __m256d c4  = _mm256_load_pd(&c[i * n + j]);
             for (k = sk; k < sk + BLOCK_SIZE; k++) {
@@ -117,13 +117,34 @@ void do_block_SIMD(int n, int si, int sj, int sk, double *a, double *b, double *
 void dgemm_blocking_SIMD(int n)
 {
     int i, j, k;
-    for(i = 0; i < n; i += BLOCK_SIZE)     
+    for(i = 0; i < n; i += BLOCK_SIZE)
         for(j = 0; j < n; j += BLOCK_SIZE)
         {
             c[i * n + j] = 0;
-            for(k = 0; k < n; k += BLOCK_SIZE)           
+            for(k = 0; k < n; k += BLOCK_SIZE)
                 do_block_SIMD(n, i, j, k, a, b, c);
         }
+}
+/*********** Loop Unrolling and SIMD Mechanism ***********/
+void dgemm_unrolling(int n)
+{
+    int i, j, k;
+    for(i = 0; i < n; i++)
+    {
+        for(j = 0; j < n; j++)
+        {
+            double cij = 0;
+            for(k = 0; k < n; k += 4)
+            {
+                double s1 = a[i * n + k] * b[k * n + j];
+                double s2 = a[i * n + (k + 1)] * b[(k + 1) * n + j];
+                double s3 = a[i * n + (k + 2)] * b[(k + 2) * n + j];
+                double s4 = a[i * n + (k + 3)] * b[(k + 3) * n + j];
+                cij += s1 + s2 + s3 + s4;
+            }
+            c[i * n + j] = cij;
+        }
+    }
 }
 
 /* Implement this function with multiple optimization techniques. */
